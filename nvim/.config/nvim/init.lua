a3afeeceb2c01cd42361f6cbfcef3d5187d79fda
Franky Chan <franky@datch.io>
2023-02-19 12:11:08 +1300

    add nvim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,10 @@
+print('init.lua loaded')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+local is_wsl = has "wsl"
+
+if is_mac then
+  require('./macos')
+end
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Avoid which. Not only is it an external process you're launching for doing very little (meaning builtins like hash, type or command are way cheaper),
+#you can also rely on the builtins to actually do what you want, while the effects of external commands can easily vary from system to system.
 if ! command -v stow &> /dev/null
 then
     echo "❗stow could not be found, installing it now.❗"
