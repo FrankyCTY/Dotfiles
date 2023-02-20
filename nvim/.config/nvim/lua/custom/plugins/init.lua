a5f5082eb46cf5bc02774e505d1f68f97164bd58
Franky Chan <franky@datch.io>
2023-02-21 02:09:08 +1300

    add nvim plugins and after lua

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,41 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {"nvim-tree/nvim-web-devicons" -- not strictly required, but recommended
+    },
+  {
+   'folke/tokyonight.nvim'
+  },
+  -- For the file icons
+  {
+    'nvim-tree/nvim-web-devicons'
+  },
+  {
+    'jose-elias-alvarez/null-ls.nvim'
+  },
+  {
+    'MunifTanjim/prettier.nvim'
+  },
+  {
+  "windwp/nvim-autopairs",
+  config = function()
+    require("nvim-autopairs").setup {}
+  end,
+  },
+  {
+  'glepnir/dashboard-nvim',
+  event = 'VimEnter',
+  -- config = function()
+  --   require('dashboard').setup {
+  --     -- config
+  --   }
+  -- end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+  },
+  {
+    'akinsho/bufferline.nvim', dependencies = {'nvim-tree/nvim-web-devicons'}
+  }
+}
